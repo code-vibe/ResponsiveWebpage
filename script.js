12811d9045d789a452daca11fc2c8e8081310614
code-vibe <mrsamoladapo@gmail.com>
2021-11-08 16:59:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// 	MENU TOGGLER
+	const hamburger = document.querySelector(".hamburger");
+	const close = document.querySelector(".close");
+	const navUL = document.querySelector(".nav-ul");
+
+	hamburger.addEventListener("click", () =>{
+		navUL.classList.toggle("show");
+		hamburger.classList.toggle("hide");
+	});
+
+	close.addEventListener("click", ()  => {
+		navUL.classList.toggle("show");
+		hamburger.classList.toggle("hide");
+	});
